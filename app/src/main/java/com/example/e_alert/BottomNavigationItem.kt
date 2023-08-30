0f105ac2d99872075a5da649bc057245406d5998
Justin Glen Vasquez <justinglenvasquez@gmail.com>
2023-08-30 22:48:19 +0800

    Navbar implemented

--- app/src/main/java/com/example/e_alert/BottomNavigationItem.kt
+++ app/src/main/java/com/example/e_alert/BottomNavigationItem.kt
@@ -1,10 +1,29 @@
 package com.example.e_alert
 
-import androidx.compose.ui.graphics.vector.ImageVector
-
 data class BottomNavigationItem(
     val title : String,
-    val selectedIcon : ImageVector,
-    val unselectedIcon : ImageVector,
+    val selectedIcon : Int,
+    val unselectedIcon : Int,
     val badgeCount : Int? = null
+)
+
+val NavItems = listOf(
+    BottomNavigationItem(
+        title = "Home",
+        selectedIcon = R.drawable.home_icon_filled_24dp,
+        unselectedIcon = R.drawable.home_icon_outlined_24dp,
+        badgeCount = null
+    ),
+    BottomNavigationItem(
+        title = "Reports",
+        selectedIcon = R.drawable.reports_icon_filled_24dp,
+        unselectedIcon = R.drawable.reports_icon_outlined_24dp,
+        badgeCount = null
+    ),
+    BottomNavigationItem(
+        title = "Routes",
+        selectedIcon = R.drawable.route_icon_filled_24dp,
+        unselectedIcon = R.drawable.route_icon_outlined_24dp,
+        badgeCount = null
+    )
 )
