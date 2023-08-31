89a7bce26ecf3a481a3d4f419ddf930d616b6197
Justin Glen Vasquez <justinglenvasquez@gmail.com>
2023-09-01 00:38:07 +0800

    Converted BottomNavBar & BottomSheet into a Composable. Still has to fix the functionality of BottomSheet (in the Home page)

--- app/src/main/java/com/example/e_alert/MainActivity.kt
+++ app/src/main/java/com/example/e_alert/MainActivity.kt
@@ -4,76 +4,123 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Badge
 import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import com.example.e_alert.ui.theme.EAlertTheme
 
 class MainActivity : ComponentActivity() {
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             EAlertTheme {
-                var selectedItemIndex by rememberSaveable {
-                    mutableStateOf(0)
-                }
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
-                ) { // Components goes here
+                ) {
                     Scaffold(
-                        bottomBar = {
-                            NavigationBar {
-                                NavItems.forEachIndexed { index, item ->
-                                    NavigationBarItem(
-                                        selected = selectedItemIndex == index,
-                                        onClick = {
-                                            selectedItemIndex = index
-                                        },
-                                        label = {
-                                            Text(text = item.title)
-                                        },
-                                        icon = {
-                                            BadgedBox(
-                                                badge = {
-                                                    if (item.badgeCount != null) {
-                                                        Badge {
-                                                            Text(text = item.badgeCount.toString())
-                                                        }
-                                                    }
-                                                }
-                                            ) {
-                                                Icon(painterResource(
-                                                    id = if (index == selectedItemIndex)
-                                                            item.selectedIcon
-                                                        else item.unselectedIcon),
-                                                    contentDescription = item.title
-                                                )
-                                            }
-                                        }
-                                    )
+                        //TODO: TopBar also goes here
+                        bottomBar = { BottomNavBar() }
+                    ) {
+                        Home()
+                    }
+                }
+            } //Theme wrapper ends here
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BottomNavBar () {
+    var selectedItemIndex by rememberSaveable {
+        mutableStateOf(0)
+    }
+
+    NavigationBar {
+        NavItems.forEachIndexed { index, item ->
+            NavigationBarItem(
+                selected = selectedItemIndex == index,
+                onClick = {
+                    selectedItemIndex = index
+                },
+                label = {
+                    Text(text = item.title)
+                },
+                icon = {
+                    BadgedBox(
+                        badge = {
+                            if (item.badgeCount != null) {
+                                Badge {
+                                    Text(text = item.badgeCount.toString())
                                 }
                             }
                         }
-                    ) { /*unused*/ }
+                    ) {
+                        Icon(painterResource(
+                            id = if (index == selectedItemIndex)
+                                item.selectedIcon
+                            else item.unselectedIcon),
+                            contentDescription = item.title
+                        )
+                    }
                 }
-            } //Theme wrapper ends here
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BottomSheet () {
+    val sheetState = rememberModalBottomSheetState()
+    var isSheetOpen by rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Button(onClick = {
+            isSheetOpen = true
+        }) {
+            Text(text = "Open sheet")
+        }
+    }
+
+    if (isSheetOpen) {
+        ModalBottomSheet(
+            sheetState = sheetState,
+            onDismissRequest = {
+                isSheetOpen = false
+            }
+        ){
+            //TODO: Here is where the Hazard Areas List goes
         }
     }
 }
