1f5c536dcd0ec6519c8009064d463b47d6beceb3
Justin Glen Vasquez <justinglenvasquez@gmail.com>
2023-09-06 02:22:49 +0800

    BottomSheet paddingValues problem solved

--- app/src/main/java/com/example/e_alert/Home.kt
+++ app/src/main/java/com/example/e_alert/Home.kt
@@ -1,18 +1,94 @@
 package com.example.e_alert
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Text
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun Home (modifier: Modifier = Modifier) {
-    BottomSheet()
+    BottomSheet (modifier = modifier) {
+        Box(
+            Modifier
+                .fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ) {
+            Card(
+                modifier =
+                Modifier
+                    .fillMaxWidth()
+                    .padding(1.dp)
+            ) {
+                ListItem(
+                    headlineContent = { Text("Three line list item") },
+                    overlineContent = { Text("OVERLINE") },
+                    supportingContent = { Text("Secondary text") },
+                    leadingContent = {
+                        Icon(
+                            Icons.Filled.Favorite,
+                            contentDescription = "Localized description",
+                        )
+                    },
+                    trailingContent = { Text("meta") }
+                )
+                ListItem(
+                    headlineContent = { Text("Three line list item") },
+                    overlineContent = { Text("OVERLINE") },
+                    supportingContent = { Text("Secondary text") },
+                    leadingContent = {
+                        Icon(
+                            Icons.Filled.Favorite,
+                            contentDescription = "Localized description",
+                        )
+                    },
+                    trailingContent = { Text("meta") }
+                )
+                ListItem(
+                    headlineContent = { Text("Three line list item") },
+                    overlineContent = { Text("OVERLINE") },
+                    supportingContent = { Text("Secondary text") },
+                    leadingContent = {
+                        Icon(
+                            Icons.Filled.Favorite,
+                            contentDescription = "Localized description",
+                        )
+                    },
+                    trailingContent = { Text("meta") }
+                )
+                ListItem(
+                    headlineContent = { Text("Three line list item") },
+                    overlineContent = { Text("OVERLINE") },
+                    supportingContent = { Text("Secondary text") },
+                    leadingContent = {
+                        Icon(
+                            Icons.Filled.Favorite,
+                            contentDescription = "Localized description",
+                        )
+                    },
+                    trailingContent = { Text("meta") }
+                )
+            }
+        }
+    }
 }
 
 @Composable
 fun Map (modifier: Modifier = Modifier) {
 
 }
+
--- app/src/main/java/com/example/e_alert/MainActivity.kt
+++ app/src/main/java/com/example/e_alert/MainActivity.kt
@@ -4,32 +4,31 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Badge
 import androidx.compose.material3.BadgedBox
-import androidx.compose.material3.Button
+import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.example.e_alert.ui.theme.EAlertTheme
 
 class MainActivity : ComponentActivity() {
@@ -43,22 +42,29 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Scaffold(
-                        //TODO: TopBar also goes here
+                        topBar = { /*TODO: TopBar()*/ },
                         bottomBar = { BottomNavBar() }
-                    ) {
-                        Home()
+                    ) { paddingValues ->
+                        Surface (
+                            Modifier
+                                .padding(paddingValues)
+                                .fillMaxSize()
+                        ) {
+                            Home()
+                        }
+
                     }
-                }
-            } //Theme wrapper ends here
-        }
-    }
+                } //Surface
+            } //EAlertTheme
+        } //setContent
+    } //onCreate
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BottomNavBar () {
     var selectedItemIndex by rememberSaveable {
-        mutableStateOf(0)
+        mutableIntStateOf(0)
     }
 
     NavigationBar {
@@ -96,31 +102,20 @@ fun BottomNavBar () {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun BottomSheet () {
+fun BottomSheet (modifier : Modifier, content: @Composable() (() -> Unit)) {
     val sheetState = rememberModalBottomSheetState()
     var isSheetOpen by rememberSaveable {
         mutableStateOf(false)
     }
+    val scaffoldState = rememberBottomSheetScaffoldState()
 
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+    BottomSheetScaffold(
+        scaffoldState = scaffoldState,
+        sheetSwipeEnabled = true,
+        sheetContent = {
+            content.invoke()
+        },
     ) {
-        Button(onClick = {
-            isSheetOpen = true
-        }) {
-            Text(text = "Open sheet")
-        }
-    }
 
-    if (isSheetOpen) {
-        ModalBottomSheet(
-            sheetState = sheetState,
-            onDismissRequest = {
-                isSheetOpen = false
-            }
-        ){
-            //TODO: Here is where the Hazard Areas List goes
-        }
     }
 }
