4d352bd889e422ac9b44bd859bf6f3349a53d32e
Justin Glen Vasquez <justinglenvasquez@gmail.com>
2023-09-21 10:40:20 +0800

    Scrollable column on ReportsPage made to work enabling multiple instances of Report; method for retrieving Report data from DB created

--- app/src/main/java/com/example/e_alert/MainActivity.kt
+++ app/src/main/java/com/example/e_alert/MainActivity.kt
@@ -15,17 +15,19 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.MaterialTheme.typography
+import androidx.compose.material3.MaterialTheme.shapes
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import com.example.e_alert.ui.theme.EAlertTheme
 
--- app/src/main/java/com/example/e_alert/ReportsPage.kt
+++ app/src/main/java/com/example/e_alert/ReportsPage.kt
@@ -1,32 +1,35 @@
 package com.example.e_alert
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.MaterialTheme.typography
+import androidx.compose.material3.MaterialTheme.shapes
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Preview
 @Composable
 fun ReportsPage (modifier: Modifier = Modifier) {
     //displays reports only if there are any from at least 1 users
-    Surface (
-        color = MaterialTheme.colorScheme.background,
-        modifier = Modifier
-    ) {
-        Report(
-            user = User(
-                name = "Justin Glen Vasquez",
-                profilePhoto = R.drawable.ic_launcher_foreground
-            ),
-            timestamp = "2 mins. ago",
-            reportPhotos = listOf(
-                R.drawable.ic_launcher_background,
-                R.drawable.ic_launcher_background
-            ),
-            reportType = "Flood",
-            reportLocation = "Sta. Cruz",
-            reportDescription = "Baha na po dito sa may Sta. Cruz, Ateneo Gate"
-        )
-    }
-}
+    Scaffold (
+        containerColor = colorScheme.surfaceColorAtElevation(3.dp)
+    ) { paddingValues ->
+        val allDataReports = retrieveReports()
+
+        LazyColumn(
+            contentPadding = paddingValues,
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            items(items = allDataReports) {
+                reportData -> Report(contents = reportData)
+            } //items
+        } //LazyColumn
+    } //Scaffold
+} //ReportsPage
